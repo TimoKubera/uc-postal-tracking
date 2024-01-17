29d537615850b36db50e726b78b4f7c2ab9f9be2
Timo Kubera <t.kubera@reply.de>
2024-01-17 11:16:55 +0100

    Creating merge conflict

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from . import auth, models, schemas
 
-app = FastAPI()
+new_app_name = FastAPI()
 
-@app.get("/status")
+@new_app_name.get("/status")
 def read_status():
     return {"status": "Service is up and running"}
 
-@app.post("/token", response_model=schemas.Token)
+@new_app_name.post("/token", response_model=schemas.Token)
 async def login_for_access_token(form_data: schemas.TokenRequestForm = Depends()):
     user = auth.authenticate_user(form_data.username, form_data.password)
     if not user:
@@ -19,6 +19,6 @@ async def login_for_access_token(form_data: schemas.TokenRequestForm = Depends()
     access_token = auth.create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
 
-@app.get("/users/me", response_model=schemas.User)
+@new_app_name.get("/users/me", response_model=schemas.User)
 async def read_users_me(current_user: models.User = Depends(auth.get_current_user)):
     return current_user
